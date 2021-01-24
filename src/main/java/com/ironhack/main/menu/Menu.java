b5f36d7670dc59eae63c61be878688e4bd2bbeab
Alejandro <amnaredo@gmail.com>
2021-01-24 13:01:03 +0100

    Moved ckeck command implementation from menu to the Command enum definition

--- src/main/java/com/ironhack/main/menu/Menu.java
+++ src/main/java/com/ironhack/main/menu/Menu.java
@@ -28,6 +28,7 @@ public class Menu {
             if (userInput.isEmpty()) continue;
 
             Command command = checkCommand(userInput);
+
             if (command.equals(Command.UNKNOWN)) continue;
 
             String[] inputArgs = getArgsFromInput(userInput);
@@ -37,7 +38,7 @@ public class Menu {
                     break;
 
                 case LOOKUP_LEAD:
-                    int idToLookup = Integer.parseInt(inputArgs[2]);
+                    int idToLookup = command.getArg(inputArgs);
                     Lead lead = lookupLead(idToLookup);
                     System.out.println(lead);
                     break;
@@ -47,18 +48,18 @@ public class Menu {
                     break;
 
                 case CONVERT_LEAD:
-                    int idToConvert = Integer.parseInt(inputArgs[1]);
+                    int idToConvert = command.getArg(inputArgs);
                     Account account = convertLead(idToConvert);
                     // todo accountMap.put(account.getId(), account);
                     break;
 
                 case CLOSE_WON_OPP:
-                    int idToCloseWon = Integer.parseInt(inputArgs[1]);
+                    int idToCloseWon = command.getArg(inputArgs);
                     closeWonOpp(idToCloseWon);
                     break;
 
                 case CLOSE_LOST_OPP:
-                    int idToCloseLost = Integer.parseInt(inputArgs[1]);
+                    int idToCloseLost = command.getArg(inputArgs);
                     closeLostOpp(idToCloseLost);
                     break;
 
@@ -88,79 +89,82 @@ public class Menu {
 
     public Command checkCommand(String userInput) {
 
-        String[] inputArgs = getArgsFromInput(userInput);
+        return Command.getCommand(userInput);
 
-        try {
-            // at least two args are present
-            if (inputArgs.length < 2) {
-                // except the special commands help, exit
-                if (inputArgs[0].equals(Keyword.HELP))
-                    return Command.HELP;
-                if (inputArgs[0].equals(Keyword.EXIT))
-                    return Command.EXIT;
-                throw new IllegalArgumentException();
-            }
-
-            // first we check the second input string
-            switch (inputArgs[1]) {
-                case Keyword.LEAD:
-                    switch (inputArgs[0]) {
-                        case Keyword.NEW:
-                            if (inputArgs.length <= 2)
-                                return Command.NEW_LEAD;
-                            throw new IllegalArgumentException();
-                        case Keyword.LOOKUP:
-                            if (inputArgs.length <= 3) {
-                                Integer.parseInt(inputArgs[2]);
-                                return Command.LOOKUP_LEAD;
-                            }
-                            throw new IllegalArgumentException();
-                        default:
-                            throw new IllegalArgumentException();
-                    }
-
-                case Keyword.LEADS:
-                    if (inputArgs[0].equals(Keyword.SHOW) && inputArgs.length <= 2)
-                        return Command.SHOW_LEADS;
-                    throw new IllegalArgumentException();
-
-                default:
-                    break;
-            }
-
-            // then we check the first input string
-            switch (inputArgs[0]) {
-                case Keyword.CONVERT:
-                    if (inputArgs.length <= 2) {
-                        Integer.parseInt(inputArgs[1]);
-                        return Command.CONVERT_LEAD;
-                    }
-                    throw new IllegalArgumentException();
-
-                case Keyword.CLOSE_WON:
-                    if (inputArgs.length <= 2){
-                        Integer.parseInt(inputArgs[1]);
-                        return Command.CLOSE_WON_OPP;
-                    }
-                    throw new IllegalArgumentException();
-
-                case Keyword.CLOSE_LOST:
-                    if (inputArgs.length <= 2) {
-                        Integer.parseInt(inputArgs[1]);
-                        return Command.CLOSE_LOST_OPP;
-                    }
-                    throw new IllegalArgumentException();
-
-                default:
-                    throw new IllegalArgumentException();
-            }
-        }
-        catch (Exception e)
-        {
-            //System.err.println(userInput + ": unknown command");
-            System.out.println(userInput + ": unknown command");
-        }
-        return Command.UNKNOWN;
+//        String[] inputArgs = getArgsFromInput(userInput);
+//
+//        try {
+//            // at least two args are present
+//            if (inputArgs.length < 2) {
+//                // except the special commands help, exit
+//                if (inputArgs[0].equals(Keyword.HELP))
+//                    return Command.HELP;
+//                if (inputArgs[0].equals(Keyword.EXIT))
+//                    return Command.EXIT;
+//
+//                throw new IllegalArgumentException();
+//            }
+//
+//            // first we check the second input string
+//            switch (inputArgs[1]) {
+//                case Keyword.LEAD:
+//                    switch (inputArgs[0]) {
+//                        case Keyword.NEW:
+//                            if (inputArgs.length <= 2)
+//                                return Command.NEW_LEAD;
+//                            throw new IllegalArgumentException();
+//                        case Keyword.LOOKUP:
+//                            if (inputArgs.length <= 3) {
+//                                Integer.parseInt(inputArgs[2]);
+//                                return Command.LOOKUP_LEAD;
+//                            }
+//                            throw new IllegalArgumentException();
+//                        default:
+//                            throw new IllegalArgumentException();
+//                    }
+//
+//                case Keyword.LEADS:
+//                    if (inputArgs[0].equals(Keyword.SHOW) && inputArgs.length <= 2)
+//                        return Command.SHOW_LEADS;
+//                    throw new IllegalArgumentException();
+//
+//                default:
+//                    break;
+//            }
+//
+//            // then we check the first input string
+//            switch (inputArgs[0]) {
+//                case Keyword.CONVERT:
+//                    if (inputArgs.length <= 2) {
+//                        Integer.parseInt(inputArgs[1]);
+//                        return Command.CONVERT_LEAD;
+//                    }
+//                    throw new IllegalArgumentException();
+//
+//                case Keyword.CLOSE_WON:
+//                    if (inputArgs.length <= 2){
+//                        Integer.parseInt(inputArgs[1]);
+//                        return Command.CLOSE_WON_OPP;
+//                    }
+//                    throw new IllegalArgumentException();
+//
+//                case Keyword.CLOSE_LOST:
+//                    if (inputArgs.length <= 2) {
+//                        Integer.parseInt(inputArgs[1]);
+//                        return Command.CLOSE_LOST_OPP;
+//                    }
+//                    throw new IllegalArgumentException();
+//
+//                default:
+//                    throw new IllegalArgumentException();
+//            }
+//        }
+//        catch (Exception e)
+//        {
+//            //System.err.println(userInput + ": unknown command");
+//            System.out.println(userInput + ": unknown command");
+//        }
+//        return Command.UNKNOWN;
     }
 
     private Lead newLead() {
--- src/main/java/com/ironhack/main/menu/command/Command.java
+++ src/main/java/com/ironhack/main/menu/command/Command.java
@@ -1,13 +1,69 @@
 package com.ironhack.main.menu.command;
 
 public enum Command {
-    UNKNOWN,
-    NEW_LEAD,
-    SHOW_LEADS,
-    LOOKUP_LEAD,
-    CONVERT_LEAD,
-    CLOSE_LOST_OPP,
-    CLOSE_WON_OPP,
-    HELP,
-    EXIT
+    UNKNOWN("", 0),
+    NEW_LEAD(Keyword.NEW + " " + Keyword.LEAD, 0),
+    SHOW_LEADS(Keyword.SHOW + " " + Keyword.LEADS, 0),
+    LOOKUP_LEAD(Keyword.LOOKUP + " " + Keyword.LEAD, 1),
+    CONVERT_LEAD(Keyword.CONVERT, 1),
+    CLOSE_LOST_OPP(Keyword.CLOSE_LOST, 1),
+    CLOSE_WON_OPP(Keyword.CLOSE_WON, 1),
+    HELP(Keyword.HELP, 0),
+    EXIT(Keyword.EXIT, 0);
+
+    private final String symbol;
+    private final int nArgs;
+
+    private Command(String symbol, int nArgs) {
+        this.symbol = symbol;
+        this.nArgs = nArgs;
+    }
+
+    private String getSymbol() {
+        return this.symbol;
+    }
+
+    private int length() {
+        return symbol.split(" ").length;
+    }
+
+    private int nArgs() {
+        return this.nArgs;
+    }
+
+    private boolean hasArgs() {
+        return this.nArgs > 0;
+    }
+
+    public int getArg(String[] inputArgs) {
+        return Integer.parseInt(inputArgs[length() + nArgs() - 1]);
+    }
+
+    public static Command getCommand(String userInput) {
+        String[] inputArgs = userInput.toUpperCase().trim().split(" +");
+        String normalizedInput = "";
+        for (String token : inputArgs)
+            normalizedInput += token + " ";
+        normalizedInput = normalizedInput.trim();
+
+        try {
+            // look for a command that matches the user input
+            for(Command command : values()) {
+
+                // skip the generic unknown command
+                if (command.equals(UNKNOWN)) continue;
+
+                if (normalizedInput.startsWith(command.getSymbol()) &&
+                    inputArgs.length == command.length() + command.nArgs()) {
+                    if (command.hasArgs())
+                        command.getArg(inputArgs);
+                    return command;
+                }
+            }
+        }
+        catch (Exception e) {
+            return UNKNOWN;
+        }
+        return UNKNOWN;
+    }
 }
