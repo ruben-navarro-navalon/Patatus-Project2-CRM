faaf51657da9bddae1bc7b06b475dc04f15bda2f
Rubén Navarro Navalón <rnavarronavalon@gmail.com>
2021-01-24 11:47:30 +0100

    Added help advertise on start.

--- src/main/java/com/ironhack/main/Main.java
+++ src/main/java/com/ironhack/main/Main.java
@@ -5,6 +5,7 @@ import com.ironhack.main.menu.Menu;
 public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
+        System.out.println("CRM Console. Type HELP for a command list.");
         menu.show();
     }
 }
